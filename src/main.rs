7ca12d631186b45f907094c4a10040fee76f6ade
Sebastian Richter <sebastian.richter@paessler.com>
2020-11-06 17:48:55 +0100

    -fine tune values to get thru pipes. -added another state which resets the birds and deletes the pipes in paused state.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use amethyst::assets::{AssetStorage, Loader};
 use amethyst::core::ecs::{
-  Builder, Component, DenseVecStorage, Entities, Join, NullStorage, Read, ReadStorage, System,
-  SystemData, World, WorldExt, Write, WriteStorage,
+  Builder, Component, DenseVecStorage, Dispatcher, DispatcherBuilder, Entities, Join, NullStorage,
+  Read, ReadStorage, System, SystemData, World, WorldExt, Write, WriteStorage,
 };
 use amethyst::core::math::Vector3;
 use amethyst::core::{EventReader, Time, Transform, TransformBundle};
@@ -26,18 +26,19 @@ use rand::{thread_rng, Rng};
 const VIRTUAL_WIDTH: f32 = 512.;
 const VIRTUAL_HEIGHT: f32 = 288.;
 const GROUND_HEIGHT: f32 = 12.;
+const GROUND_WIDTH: f32 = 1100.;
 const BACKGROUND_SCROLL_SPEED: f32 = 30.;
-const GROUND_SCROLL_SPEED: f32 = 60.;
+const GROUND_SCROLL_SPEED: f32 = 61.;
 const BACKGROUND_LOOPING_POINT: f32 = 413.;
 const BACKGROUND_LOOPING_OFFSET: f32 = 290.;
-const BIRD_GRAVITY: f32 = -30.;
+const BIRD_GRAVITY: f32 = -26.;
 const BIRD_WIDTH: f32 = 38.;
 const BIRD_HEIGHT: f32 = 24.;
-const BIRD_JUMP: f32 = 5.;
+const BIRD_JUMP: f32 = 4.;
 const PIPE_SCROLL: f32 = -60.;
 const PIPE_WIDTH: f32 = 70.;
 const PIPE_HEIGHT: f32 = 288.;
-const PIPE_GAP: f32 = 90.;
+const PIPE_GAP: f32 = 110.;
 
 #[derive(Debug)]
 enum BackgroundType {
@@ -124,9 +125,7 @@ impl<'a> System<'a> for BirdSystem {
       if input.key_is_down(VirtualKeyCode::Space) {
         bird.dy = BIRD_JUMP;
       }
-      if transform.translation().y > -VIRTUAL_HEIGHT / 2. {
-        transform.prepend_translation_y(bird.dy);
-      }
+      transform.prepend_translation_y(bird.dy);
     }
   }
 }
@@ -158,12 +157,13 @@ struct CollisionSystem;
 impl<'a> System<'a> for CollisionSystem {
   type SystemData = (
     ReadStorage<'a, Bird>,
+    ReadStorage<'a, Background>,
     ReadStorage<'a, Pipe>,
     ReadStorage<'a, Transform>,
     Write<'a, EventChannel<GameEvent>>,
   );
 
-  fn run(&mut self, (birds, pipes, transforms, mut event_ch): Self::SystemData) {
+  fn run(&mut self, (birds, backgrounds, pipes, transforms, mut event_ch): Self::SystemData) {
     for (_, transform) in (&birds, &transforms).join() {
       let bird_x = transform.translation().x;
       let bird_y = transform.translation().y;
@@ -183,20 +183,56 @@ impl<'a> System<'a> for CollisionSystem {
           event_ch.single_write(GameEvent::Collision);
         }
       }
+
+      for (background, transform) in (&backgrounds, &transforms).join() {
+        match background.b_type {
+          BackgroundType::Background => {}
+          BackgroundType::Ground => {
+            let background_x = transform.translation().y - (GROUND_WIDTH / 2.);
+            let background_y = transform.translation().y - (GROUND_HEIGHT / 2.);
+
+            if point_in_rect(
+              bird_x,
+              bird_y,
+              background_x - BIRD_WIDTH / 2.,
+              background_y - BIRD_HEIGHT / 2.,
+              background_x + GROUND_WIDTH + BIRD_WIDTH / 2.,
+              background_y + GROUND_HEIGHT + BIRD_HEIGHT / 2.,
+            ) {
+              event_ch.single_write(GameEvent::Collision);
+            }
+          }
+        }
+      }
     }
   }
 }
 
 #[derive(Default)]
-struct PlayState {
+struct PlayState<'a, 'b> {
   pipe_spawn_timer: Option<f32>,
   pipe_sprite: Option<SpriteRender>,
   rand: Option<ThreadRng>,
+  dispatcher: Option<Dispatcher<'a, 'b>>,
 }
 
-impl<'a, 'b> State<GameData<'a, 'b>, MyStateEvent> for PlayState {
+impl<'a, 'b> State<GameData<'a, 'b>, MyStateEvent> for PlayState<'a, 'b> {
   fn on_start(&mut self, _data: StateData<'_, GameData<'_, '_>>) {
     let world = _data.world;
+
+    let mut dispatcher_builder = DispatcherBuilder::new();
+    dispatcher_builder.add(BackgroundSystem, "background_system", &[]);
+    dispatcher_builder.add(BirdSystem, "bird_system", &[]);
+    dispatcher_builder.add(PipeSystem, "pipe_system", &[]);
+    dispatcher_builder.add(
+      CollisionSystem,
+      "collision_system",
+      &["bird_system", "pipe_system"],
+    );
+    let mut dispatcher = dispatcher_builder.build();
+    dispatcher.setup(world);
+    self.dispatcher = Some(dispatcher);
+
     let background_sprite =
       load_sprite("texture/background.png", "texture/background.ron", 0, world);
     let ground_sprite = load_sprite("texture/ground.png", "texture/ground.ron", 0, world);
@@ -253,7 +289,7 @@ impl<'a, 'b> State<GameData<'a, 'b>, MyStateEvent> for PlayState {
       }
     }
     if let MyStateEvent::Game(GameEvent::Collision) = event {
-      println!("collision");
+      return Trans::Push(Box::new(PauseState));
     }
     Trans::None
   }
@@ -304,6 +340,53 @@ impl<'a, 'b> State<GameData<'a, 'b>, MyStateEvent> for PlayState {
         self.pipe_spawn_timer.replace(timer);
       }
     }
+
+    if let Some(dispatcher) = self.dispatcher.as_mut() {
+      dispatcher.dispatch(&data.world);
+    }
+    data.data.update(&data.world);
+    Trans::None
+  }
+}
+
+#[derive(Default)]
+struct PauseState;
+
+impl<'a, 'b> State<GameData<'a, 'b>, MyStateEvent> for PauseState {
+  fn handle_event(
+    &mut self,
+    data: StateData<'_, GameData<'a, 'b>>,
+    event: MyStateEvent<StringBindings>,
+  ) -> Trans<GameData<'a, 'b>, MyStateEvent<StringBindings>> {
+    if let MyStateEvent::Window(event) = &event {
+      if is_key_down(&event, VirtualKeyCode::Space) {
+        let entities = data.world.entities();
+        let pipes = data.world.read_storage::<Pipe>();
+        let birds = data.world.read_storage::<Bird>();
+        let mut transforms = data.world.write_storage::<Transform>();
+        for (_, transform) in (&birds, &mut transforms).join() {
+          transform.set_translation(Vector3::new(0., 0., 4.));
+        }
+        for (e, _) in (&entities, &pipes).join() {
+          entities
+            .delete(e)
+            .expect("Couldn't delete pipe entity while state was paused!");
+        }
+        Trans::Pop
+      } else if is_close_requested(&event) || is_key_down(&event, VirtualKeyCode::Escape) {
+        Trans::Quit
+      } else {
+        Trans::None
+      }
+    } else {
+      Trans::None
+    }
+  }
+
+  fn update(
+    &mut self,
+    data: StateData<'_, GameData<'a, 'b>>,
+  ) -> Trans<GameData<'a, 'b>, MyStateEvent<StringBindings>> {
     data.data.update(&data.world);
     Trans::None
   }
@@ -353,14 +436,6 @@ fn main() -> amethyst::Result<()> {
   let assets_dir = app_root.join("assets");
 
   let game_data = GameDataBuilder::default()
-    .with(BackgroundSystem, "background_system", &[])
-    .with(BirdSystem, "bird_system", &[])
-    .with(PipeSystem, "pipe_system", &[])
-    .with(
-      CollisionSystem,
-      "collision_system",
-      &["bird_system", "pipe_system"],
-    )
     .with_bundle(TransformBundle::new())?
     .with_bundle(InputBundle::<StringBindings>::new())?
     .with_bundle(
